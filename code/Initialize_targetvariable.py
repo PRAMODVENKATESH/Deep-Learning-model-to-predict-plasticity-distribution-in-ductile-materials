6c042b6f70ee3335f37483d84c7035368a9762b3
PRAMODVENKATESH <78496229+PRAMODVENKATESH@users.noreply.github.com>
2021-10-18 14:46:41 +0200

    Create Initialize_targetvariable.py

---
+++ code/Initialize_targetvariable.py
@@ -0,0 +1,11 @@
+import 
+def Total_reaction_force_data(inputPath1):
+# initialize the list of column names in the CSV file and then
+# load it using Pandas
+    cols = ["Totalreactionforce"]
+    df = pd.read_csv(inputPath1, sep=",", header=None, names=cols)
+    print(df.shape)
+    #print(df.head(3))
+    print(df.describe)
+	  #return the data frame
+    return df
